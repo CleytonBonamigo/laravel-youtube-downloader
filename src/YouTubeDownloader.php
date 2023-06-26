d708c77bcfdeb280799d09c6aad8806637b0d1a6
Cleyton Bonamigo <cleytonbonamigo@gmail.com>
2023-06-26 07:28:00 -0300

    New instance for the class to test the composer load.

--- src/YouTubeDownloader.php
+++ src/YouTubeDownloader.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace YouTube;
+
+class YouTubeDownloader
+{
+    public function __construct()
+    {
+
+    }
+
+    public function greetings(String $name): string
+    {
+        return "Hi {$name}! Hope you are having an awesome day!";
+    }
+}
